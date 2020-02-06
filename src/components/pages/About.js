7753938d623a1fde6dd75d740c332b37c084ac88
Nick Filippakis <nick.filippakis@ibm.com>
2020-02-06 14:20:42 +0000

    added link to crash course

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function About() {
     return (
         <React.Fragment>
            <h1>About</h1>
            <p>This is the TodoList app v1.0.0. It is part of a React.js crash course</p> 
+           <p>To find this crash course, please click <a 
+                                                        style={{color: 'blue', textDecoration: 'underline'}} 
+                                                        href="https://www.youtube.com/watch?v=sBws8MSXN7A"
+                                                        title="React JS Crash Course"
+                                                        target="_blank">here</a></p>
         </React.Fragment>
     )
 }
